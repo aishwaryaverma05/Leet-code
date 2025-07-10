8b33b2ec16b4e0ddc8cc6675b81a7038f4f0a280
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-07-10 10:46:19 +0530

    Time: 19 ms (30.58%) | Memory: 14.2 MB (30.67%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char,int>mapIndex;
-        int i=0;
-        int maxlen=0;
-        for(int j=0;j<s.size();j++){
-            char current=s[j];
-            if(mapIndex.find(current) != mapIndex.end() && mapIndex[current]>=i){
-                i=mapIndex[current]+1;
+        //     unordered_map<char,int>mapIndex;
+        //     int i=0;
+        //     int maxlen=0;
+        //     for(int j=0;j<s.size();j++){
+        //         char current=s[j];
+        //         if(mapIndex.find(current) != mapIndex.end() &&
+        //         mapIndex[current]>=i){
+        //             i=mapIndex[current]+1;
+        //         }
+        //         mapIndex[current]=j;
+        //         maxlen=max(maxlen,j-i+1);
+        //     }
+        // return maxlen;
+        int n = s.length();
+        int left = 0;
+        int maxlen = 0;
+        unordered_set<char>charset;
+        for(int right=0;right<n;right++){
+            while(charset.find(s[right])!= charset.end()){
+                charset.erase(s[left]);
+                left++;
             }
-            mapIndex[current]=j;
-            maxlen=max(maxlen,j-i+1);
+            charset.insert(s[right]);
+            maxlen=max(maxlen,right-left+1);
         }
-    return maxlen;
+        return maxlen;
     }
 };
