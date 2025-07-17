20dff31c651af8e0b939b15cc56afdc7441224e3
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-07-17 17:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (45.51%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        while(stones.size()>1){
-            sort(stones.begin(),stones.end(),greater<int>());
+        // brute
+        // while(stones.size()>1){
+        //     sort(stones.begin(),stones.end(),greater<int>());
 
-            int x=stones[0];
-            int y=stones[1];
+        //     int x=stones[0];
+        //     int y=stones[1];
 
-            // stones.erase(stones.begin(),stones.end()+2);
-            stones.erase(stones.begin(), stones.begin() + 2);
+        //     // stones.erase(stones.begin(),stones.end()+2);
+        //     stones.erase(stones.begin(), stones.begin() + 2);
+
+        //     if(x!=y){
+        //         // stones.push_back(abs(x-y));
+        //         stones.push_back(abs(x - y));
+        //     }
+        // }
+        // return stones.empty()? 0:stones[0];
+
+
+
+        // optimal
+        priority_queue<int> maxHeap(stones.begin(),stones.end());
+
+        while(maxHeap.size()>1){
+            
+            int x=maxHeap.top();maxHeap.pop();
+            int y=maxHeap.top();maxHeap.pop();
 
             if(x!=y){
-                // stones.push_back(abs(x-y));
-                stones.push_back(abs(x - y));
+                maxHeap.push(abs(x-y));
             }
         }
-        return stones.empty()? 0:stones[0];
+        return maxHeap.empty() ? 0 : maxHeap.top();
     }
 };
