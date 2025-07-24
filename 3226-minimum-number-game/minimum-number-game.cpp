41e47108ed5fbde6f5c1a4410acbe4a4d4b08fbe
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-07-24 18:59:48 +0530

    Time: 2 ms (28.02%) | Memory: 28.4 MB (22.59%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -11,16 +11,30 @@ public:
 
         // return nums;
 
+        // ANOTHER METHOD
+        // sort(nums.begin(), nums.end());
+        // vector<int> result;
+    
+        // for (int i = 0; i < nums.size(); i += 2) {
+        // // Push the larger then smaller in each pair
+        //     result.push_back(nums[i+1]);
+        //     result.push_back(nums[i]);
+        // }
+    
+        // return result;
 
-        sort(nums.begin(), nums.end());
-        vector<int> result;
-    
-        for (int i = 0; i < nums.size(); i += 2) {
-        // Push the larger then smaller in each pair
-            result.push_back(nums[i+1]);
-            result.push_back(nums[i]);
+
+        // Heap
+        priority_queue<int,vector<int>,greater<int>>minHeap(nums.begin(),nums.end());
+
+        vector<int>arr;
+        while(!minHeap.empty()){
+            int alice=minHeap.top();minHeap.pop();
+            int Bob=minHeap.top();minHeap.pop();
+
+            arr.push_back(Bob);
+            arr.push_back(alice);
         }
-    
-        return result;
+        return arr;
     }
 };
