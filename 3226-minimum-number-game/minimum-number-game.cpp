22b6263d63753f52289e67397af87ba7e11211c2
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-07-24 18:54:15 +0530

    Time: 3 ms (23.81%) | Memory: 28.3 MB (40.54%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -3,12 +3,24 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         // brute
         // 1.sort the array
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // 2.swap 
+        // for(int i=0;i<nums.size();i+=2){
+        //     swap(nums[i],nums[i+1]);
+        // }
 
-        for(int i=0;i<nums.size();i+=2){
-            swap(nums[i],nums[i+1]);
+        // return nums;
+
+
+        sort(nums.begin(), nums.end());
+        vector<int> result;
+    
+        for (int i = 0; i < nums.size(); i += 2) {
+        // Push the larger then smaller in each pair
+            result.push_back(nums[i+1]);
+            result.push_back(nums[i]);
         }
-
-        return nums;
+    
+        return result;
     }
 };
