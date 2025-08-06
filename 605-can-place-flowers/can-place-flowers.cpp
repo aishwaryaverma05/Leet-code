9e1d27d76b0bf4da189f5069a551dcfcc810b2a0
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-08-06 07:33:25 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (5.75%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,38 +1,19 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        // int sz=flowerbed.size();
-        // for(int i=1;i<sz-1;i++){
-        //     if(flowerbed[i]==1) i++;
-            
-        //     else{  //i=0
-        //         if((flowerbed[i-1]==0) && (flowerbed[i+1]==0)){
-        //             flowerbed[i]=1;
-        //             n--;
-        //         }
-        //         else if(flowerbed[i]==0 && flowerbed[i-1]==0){
-        //             flowerbed[i]=1;
-        //             n--;
-        //         }
-        //     }
-        // }
-        // return n==0 ? true:false;
+    bool canPlaceFlowers(vector<int>& fb, int n) {
+        int sz=fb.size();
+        int cnt=0;
+        for(int i=0;i<sz;i++){
+            if(fb[i]==0){
+                bool left= (i==0) || (fb[i-1]==0);
+                bool right= (i==sz-1) || (fb[i+1]==0);
 
-        int sz = flowerbed.size();
-        for (int i = 0; i < sz && n > 0; i++) {
-            if (flowerbed[i] == 0) {
-                bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
-                bool rightEmpty = (i == sz - 1) || (flowerbed[i + 1] == 0);
-                
-                if (leftEmpty && rightEmpty) {
-                    flowerbed[i] = 1; 
-                    n--;              
-                    i++;             
+                if(left && right){
+                    fb[i]=1;
+                    cnt++;
                 }
-            } else {
-                i++; 
             }
         }
-        return n == 0;
+        return (cnt>=n);
     }
 };
