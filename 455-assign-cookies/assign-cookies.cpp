163d90a0aca8e53d651ae0f5a3314813989bece9
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-08-06 06:02:49 +0530

    Time: 11 ms (42.01%) | Memory: 44.9 MB (10.02%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int content=0,i=0,j=0;
+        int i=0,j=0,content=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        while (i < g.size() && j < s.size()) {
-            if (s[j] >= g[i]) {  
-                content++; 
-                i++;       
+        while(i<g.size() && j<s.size()){
+            if(s[j]>=g[i]){
+                content++;
+                i++;
             }
-            j++; 
+            j++;
         }
         return content;
     }
