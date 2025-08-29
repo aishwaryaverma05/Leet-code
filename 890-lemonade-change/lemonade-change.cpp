4a5a5e5783a4ebc195b0e5042b62203403f72786
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-08-29 14:09:29 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (93.75%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int five = 0, ten = 0;
-
-        for (int bill : bills) {
-            if (bill == 5) {
+        int five=0,ten=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5){
                 five++;
-            } else if (bill == 10) {
-                if (five == 0)
-                    return false;
-                five--;
+            }
+            else if(bills[i]==10){
+                if(five==0) return false;
                 ten++;
-            } else {
-                if (ten > 0 && five > 0) {
+                five--;
+            }
+            else{
+                if(ten>0 && five>0){
                     ten--;
                     five--;
-                } else if (five >= 3) {
-                    five -= 3;
-                } else {
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
                     return false;
                 }
             }
         }
-
         return true;
     }
 };
