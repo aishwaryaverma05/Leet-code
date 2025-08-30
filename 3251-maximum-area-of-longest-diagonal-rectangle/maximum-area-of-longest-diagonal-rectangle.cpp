09cb2d1b3d18eabab2b4b64f8e2c5bf9c5b7954e
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-08-31 01:04:09 +0530

    Time: 1 ms (21.80%) | Memory: 29.9 MB (10.03%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,36 +1,20 @@
-// class Solution {
-// public:
-//     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-//         int f_area=0; 
-//         int f_diag=0;
-//         for(auto pair:dimensions){
-//             int ar=pair[0]*pair[1];
-//             int diag = sqrt(pair[0]*pair[0] + pair[1]*pair[1]);
-//             f_diag=max(f_diag,diag);
-//             f_area=max(f_area,ar);
-//         }
-//         return f_area;
-//     }
-// };
-
-
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int f_area=0;
-        double f_diag=0;
-        for(auto pair:dimensions){
-            int ar=pair[0]*pair[1];
-            double diag = sqrt(pair[0]*pair[0] + pair[1]*pair[1]);
-            if(f_diag<diag){
-                f_diag=diag;
-                f_area=ar;
+        int max_area=0;
+        double max_dia=0;
+        for(auto p:dimensions){
+            double dia=sqrt(p[0]*p[0]+p[1]*p[1]);
+            // max_dia=max(max_dia,dia);
+            int area=p[0]*p[1];
+            if(dia>max_dia){
+                max_dia=dia;
+                max_area=area;
             }
-            else if(f_diag==diag){
-                f_area=max(f_area,ar);
+            else if(max_dia==dia){
+                max_area=max(max_area,area);
             }
-            
         }
-        return f_area;
+        return max_area;
     }
 };
