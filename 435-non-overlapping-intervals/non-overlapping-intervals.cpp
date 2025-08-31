923a95090575d191b0e4e978315c64ff0c795527
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2025-09-01 00:40:53 +0530

    Time: 36 ms (91.84%) | Memory: 94 MB (39.57%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[] (const vector<int>& a,const vector<int>& b){
-            return a[1]<b[1];
-        });
-
-        // int removed=0;
-        // for(auto i=1;i<intervals.size();i++){
-        //     if(intervals[i][0]>=intervals[i-1][1]){
-        //         continue;
-        //     }
-        //     else{
-        //         removed++;
-        //     }
-        // }
-        // return removed;
-
-        int cnt=1;  // maan ke chal rahe hai ki first meeting toh ho hi rahi hai
-        int endTime=intervals[0][1];
-        int sz=intervals.size();
-        for(int i=1;i<sz;i++){
-            if(intervals[i][0]>=endTime){
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a,const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+        int cnt = 0;
+        int endtime=intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0]>=endtime) {
+                endtime=intervals[i][1];
+                // continue;
+            } else {
                 cnt++;
-                endTime=intervals[i][1];
             }
         }
-
-        return sz-cnt;
+        return cnt;
     }
 };
