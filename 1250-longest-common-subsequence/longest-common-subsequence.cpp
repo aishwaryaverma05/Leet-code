b11c30dc35b15b925fd93424f77172887556856b
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-01-04 07:38:25 +0530

    Time: 23 ms (80.52%) | Memory: 27.3 MB (67.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -22,8 +22,8 @@ public:
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 if (text1[i - 1] == text2[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
