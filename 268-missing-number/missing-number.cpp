c4570b575172a94c24f511949b75a0430491f201
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-07-03 16:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (38.13%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]!=i) return i;
+        // }
+        // return n;
+        int XOR1=0;
+        int XOR2=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]!=i) return i;
+            XOR1^=nums[i];
+            XOR2^=i;
         }
-        return n;
+        XOR2^=n;
+        return XOR1^XOR2;
     }
 };
