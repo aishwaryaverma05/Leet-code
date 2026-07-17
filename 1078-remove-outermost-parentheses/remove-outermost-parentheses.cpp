bfc6f9417145965179e61ef0e71498638d808db6
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-07-17 09:52:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (55.01%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        stack<int>st;
+        // stack<int>st;
+        int depth=0;
         string ans="";
         for(char ch:s){
             if(ch=='('){
-                if(!st.empty()){
+                if(depth>0){
                     ans+=ch;
                 }
-                st.push(ch);
+                // st.push(ch);
+                depth++;
             }
             else{
-                st.pop();
-                if(!st.empty()){
+                // st.pop();
+                depth--;
+                if(depth>0){
                     ans+=ch;
                 }
             }
