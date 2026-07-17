95ac50a2ca3ee65497dbbafcba58dce40f120560
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-07-17 10:41:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (16.60%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>mpp;
+        for(char ch:s){
+            mpp[ch]++;
+        }
+        vector<pair<char,int>>vec(mpp.begin(),mpp.end());
+        sort(vec.begin(),vec.end(),[]( auto a, auto b){
+            return a.second>b.second;
+        });
+        string res="";
+        for(const auto &it:vec){
+            res+=string(it.second,it.first);
+        }
+        return res;
+    }
+};
