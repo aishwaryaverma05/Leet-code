a511d42941d1258640dc68486c8710811d542750
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-08-25 00:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (74.46%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,18 +11,15 @@
  */
 class Solution {
 public:
+    void postorder(TreeNode* root,vector<int>& vec){
+        if(root==NULL) return;
+        postorder(root->left,vec);
+        postorder(root->right,vec);
+        vec.push_back(root->val);
+    }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>vec;
         postorder(root,vec);
         return vec;
-    }
-private:
-    void postorder(TreeNode *root,vector<int> &vec){
-        if(root==NULL){
-            return;
-        }
-        postorder(root->left,vec);
-        postorder(root->right,vec);
-        vec.push_back(root->val);
     }
 };
