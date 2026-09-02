7a28edf8ec28e8da56860dba1f7a7e6e506f55d8
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-09-02 16:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (81.46%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k=n-1;
+        int k=0;
         for(int i=0;i<nums1.size();i++){
-            if(nums1[i]==0 && k!=-1){
-                nums1[i]=nums2[k--];
+            if(nums1[i]==0 && k<n){
+                nums1[i]=nums2[k++];
             }
         }
         sort(nums1.begin(),nums1.end());
