9c6fdb39c43c3e3e75ce52c8fcc8c195b4a0f86e
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-09-02 16:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (24.96%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        sort(strs.begin(),strs.end());
+        string first=strs[0];
+        string back=strs.back();
+        int i=0;
+        while(i<first.size() && i<back.size()  && first[i]==back[i]){
+            i++;
+        }
+        return first.substr(0,i);
+    }
+};
