e820cfcba4d756c28f906715d94426a229495bbe
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-09-07 11:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int func(int n,vector<int>& dp){
+    int climbStairs(int n) {
+        vector<int>dp(n+1,0);
         if(n<=2)return n;
         dp[1]=1;dp[2]=2;
 
@@ -8,10 +9,5 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-    }
-    int climbStairs(int n) {
-        vector<int>dp(n+1,0);
-        int res=func(n,dp);
-        return res;
     }
 };
