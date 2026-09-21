c72894a0505848b1cd948490761793385cc423c5
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-09-21 11:00:52 +0530

    Time: 51 ms (44.97%) | Memory: 94 MB (38.42%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
+    static bool compare(vector<int>& a,vector<int>& b){
+        if(a[1]<b[1])return true;
+        else return false;
+    }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(),
-             [](const vector<int>& a,const vector<int>& b) {
-                 return a[1] < b[1];
-             });
-        int cnt = 0;
+        sort(intervals.begin(),intervals.end(),compare);
+        int len=intervals.size();
+        int cnt=1;
         int endtime=intervals[0][1];
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0]>=endtime) {
-                endtime=intervals[i][1];
-                // continue;
-            } else {
+
+        for(int i=1;i<len;i++){
+            if(intervals[i][0]>=endtime){
                 cnt++;
+                endtime=intervals[i][1];
             }
         }
-        return cnt;
+        return len-cnt;
     }
 };
