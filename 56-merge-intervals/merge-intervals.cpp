f5983af728937089d20015e4309be32a25198adc
Aishwarya Verma <aishwarya.23bai10284@vitbhopal.ac.in>
2026-09-21 11:55:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (60.36%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
+    static bool compare(vector<int>& a , vector<int>& b){
+        if(a[0]<b[0])return true;
+        else return false;
+    }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](const vector<int>&a,const vector<int>&b){
-            return a[0]<b[0];
-        });
-        if(intervals.size()==1) return intervals;
+        sort(intervals.begin(),intervals.end(),compare);
         vector<vector<int>>vec;
-        // int start=intervals[0][0];
-        // int end=intervals[0][1];
-        // for(int i=1;i<intervals.size();i++){
-        //     if(intervals[i][0]<=end){
-        //         vec.push_back({start,intervals[i][1]});
-        //     }
-        //     else{
-        //         vec.push_back({intervals[i][0],intervals[i][1]});
-        //     }
-        //     start=vec[i-1][0];
-        //     end=vec[i-1][1];
-        // }
-        // return vec;
-
-        vector<int> temp=intervals[0];
-        for(auto pair:intervals){
-            if(pair[0]<=temp[1]){
-                temp[1]=max(temp[1],pair[1]);
+        int end=intervals[0][1];
+        vec.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=end){
+                vec.back()[1]=max(vec.back()[1],intervals[i][1]);
+                end=vec.back()[1];
             }
             else{
-                vec.push_back(temp);
-                temp=pair;
+                vec.push_back(intervals[i]);
+                end = intervals[i][1];
             }
         }
-        vec.push_back(temp);
         return vec;
-
     }
 };
